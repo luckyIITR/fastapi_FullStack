4a051ab9aaed5c1a584cab5ccd9b91f6985e6364
Lucky Kushwaha <luckykushwaha56@gmail.com>
2023-12-11 16:27:33 +0530

    basic api copy pasted

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import models
+from database import engine
+from routes import auth, todos, admin, users
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+app.include_router(auth.router)
+app.include_router(todos.router)
+app.include_router(admin.router)
+app.include_router(users.router)
