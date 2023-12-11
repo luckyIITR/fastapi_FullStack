111b88191c94864f4de32a073adad24e45110287
Lucky Kushwaha <luckykushwaha56@gmail.com>
2023-12-11 17:51:09 +0530

    create todo added, hardcoded the user

--- routes/todos.py
+++ routes/todos.py
@@ -1,12 +1,12 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException, Path, Request
+from fastapi import APIRouter, Depends, Form, Request
 from starlette import status
-from models import Todos
+from starlette.responses import RedirectResponse
+import models
 from database import SessionLocal
 from sqlalchemy.orm import Session
 from pydantic import BaseModel, Field
 from .auth import get_current_user
-
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
@@ -32,7 +32,8 @@ user_dependency = Annotated[dict, Depends(get_current_user)]
 
 @router.get("/", response_class=HTMLResponse)
 async def read_all_by_user(request: Request, db: Session = Depends(get_db)):
-    return templates.TemplateResponse("home.html", {"request": request})
+    todos = db.query(models.Todos).filter(models.Todos.owner_id == 1).all()
+    return templates.TemplateResponse("home.html", {"request": request, "todos": todos})
 
 
 @router.get("/add-todo", response_class=HTMLResponse)
@@ -40,3 +41,18 @@ async def add_new_todo(request: Request):
 
     return templates.TemplateResponse("add-todo.html", {"request": request})
 
+
+@router.post("/add-todo", response_class=HTMLResponse)
+async def create_todo(request: Request, title: str = Form(...), description: str = Form(...),
+                      priority: int = Form(...), db: Session = Depends(get_db)):
+    todo_model = models.Todos()
+    todo_model.title = title
+    todo_model.description = description
+    todo_model.priority = priority
+    todo_model.complete = False
+    todo_model.owner_id = 1
+
+    db.add(todo_model)
+    db.commit()
+
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
