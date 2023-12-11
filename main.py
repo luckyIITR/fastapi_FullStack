945e9820b723e128dc62b70c2a22a3e7dbe20387
Lucky Kushwaha <luckykushwaha56@gmail.com>
2023-12-11 21:33:51 +0530

    home page redirect

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from starlette import status
+from starlette.responses import HTMLResponse, RedirectResponse
+
 import models
 from database import engine
-from routes import auth, todos, admin, users
+from routes import auth, todos
 from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
@@ -11,5 +14,11 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
-app.include_router(admin.router)
-app.include_router(users.router)
+
+
+# app.include_router(admin.router)
+# app.include_router(users.router)
+
+@app.get("/", response_class=HTMLResponse)
+async def authentication_page(request: Request):
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
--- routes/admin.py
+++ routes/admin.py
@@ -1,5 +1,4 @@
 from typing import Annotated
-from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, HTTPException, Path
 from starlette import status
