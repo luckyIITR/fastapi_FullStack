f73f7ac61e88e6f15b37b9f721ae1202066c10fb
Lucky Kushwaha <luckykushwaha56@gmail.com>
2023-12-11 18:03:34 +0530

    edit todo

--- routes/todos.py
+++ routes/todos.py
@@ -38,7 +38,6 @@ async def read_all_by_user(request: Request, db: Session = Depends(get_db)):
 
 @router.get("/add-todo", response_class=HTMLResponse)
 async def add_new_todo(request: Request):
-
     return templates.TemplateResponse("add-todo.html", {"request": request})
 
 
@@ -51,6 +50,30 @@ async def create_todo(request: Request, title: str = Form(...), description: str
     todo_model.priority = priority
     todo_model.complete = False
     todo_model.owner_id = 1
+
+    db.add(todo_model)
+    db.commit()
+
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
+
+
+@router.get("/edit-todo/{todo_id}", response_class=HTMLResponse)
+async def edit_todo(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    return templates.TemplateResponse("edit-todo.html", {"request": request, "todo": todo})
+
+
+@router.post("/edit-todo/{todo_id}", response_class=HTMLResponse)
+async def edit_todo_commit(request: Request, todo_id: int, title: str = Form(...),
+                           description: str = Form(...), priority: int = Form(...),
+                           db: Session = Depends(get_db)):
+
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    todo_model.title = title
+    todo_model.description = description
+    todo_model.priority = priority
 
     db.add(todo_model)
     db.commit()
